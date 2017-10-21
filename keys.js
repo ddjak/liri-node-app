2dbf4d0a477b748d04d67e5136a9e0060cb43d94
admin <admin>
2017-10-21 01:21:56 -0400

    without

--- keys.js
+++ keys.js
@@ -1,15 +1,15 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: 'nKdVLd9d607AjUwtgTycnbtyQ',
-  consumer_secret: '1Aax9zXL9k8CNmluXy2vcNBrgcwEdktLn1RyvOMpWcuD5Oc1za',
-  access_token_key: '516201486-ApGqP19b7NyfGt50lD3l3LxLziCueLIRHDtZcs9y',
-  access_token_secret: 'Kn3H9jTXY0GmDsRSlUXEiQ98XZqssJiTMMhZXyUfMyqJD',
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: '',
 }
 
 var spotifyKeys = {
-	id: 'f7d548297eaf4259b0a535c5128762fb',
-	secret: 'e1c321e0311f4c3bbbdfddfb0e86f215'
+	id: '',
+	secret: ''
 }
 
 module.exports = [twitterKeys, spotifyKeys];
